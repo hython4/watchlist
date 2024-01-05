9c1ff20f95c8ec8dd3822ad492c7fa527579546e
hython4 <hython4@gmail.com>
2024-01-05 06:31:11 +0000

    Add index page

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask, url_for
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+name = "Hython"
+movies = [
+    {"title": "My Neighbor Totoro", "year": "1988"},
+    {"title": "Dead Poets Society", "year": "1989"},
+    {"title": "A Perfect World", "year": "1993"},
+    {"title": "Leon", "year": "1994"},
+    {"title": "Mahjong", "year": "1996"},
+    {"title": "Swallowtail Butterfly", "year": "1996"},
+    {"title": "King of Comedy", "year": "1999"},
+    {"title": "Devils on the Doorstep", "year": "1999"},
+    {"title": "WALL-E", "year": "2008"},
+    {"title": "The Pork of Music", "year": "2012"},
+]
 
 
 @app.route("/hello")
@@ -19,3 +33,8 @@ def test_url_for():
     print(url_for("user_page", name="chuang"))
     print(url_for("test_url_for", num=2))
     return "Test page"
+
+
+@app.route("/")
+def home():
+    return render_template("index.html", name=name, movies=movies)
