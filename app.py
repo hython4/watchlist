370054dc49c4ddd04262730df3a963693b303e7e
hython4 <hython4@gmail.com>
2024-01-05 07:46:55 +0000

    Add 404 template

--- app.py
+++ app.py
@@ -54,6 +54,18 @@ def index():
     return render_template("index.html", user=user, movies=movies)
 
 
+@app.context_processor
+def inject_user():  # 函数名可以随意修改
+    user = User.query.first()
+    return dict(user=user)  # 需要返回字典，等同于 return {'user': user}
+
+
+@app.errorhandler(404)  # 传入要处理的错误代码
+def page_not_found(e):  # 接受异常对象作为参数
+    user = User.query.first()
+    return render_template("404.html", user=user), 404  # 返回模板和状态码
+
+
 @app.cli.command()  # 注册为命令，可以传入 name 参数来自定义命令
 @click.option("--drop", is_flag=True, help="Create after drop.")  # 设置选项
 def initdb(drop):
