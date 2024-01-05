4cc83c198a74e049c1406b6bc01f64a87c1ac4ac
hython4 <hython4@gmail.com>
2024-01-05 07:56:07 +0000

    Add base template and error template

--- app.py
+++ app.py
@@ -47,23 +47,21 @@ def test_url_for():
     return "Test page"
 
 
-@app.route("/")
-def index():
-    user = User.query.first()
-    movies = Movie.query.all()
-    return render_template("index.html", user=user, movies=movies)
-
-
 @app.context_processor
 def inject_user():  # 函数名可以随意修改
     user = User.query.first()
     return dict(user=user)  # 需要返回字典，等同于 return {'user': user}
 
 
-@app.errorhandler(404)  # 传入要处理的错误代码
-def page_not_found(e):  # 接受异常对象作为参数
-    user = User.query.first()
-    return render_template("404.html", user=user), 404  # 返回模板和状态码
+@app.route("/")
+def index():
+    movies = Movie.query.all()
+    return render_template("index.html", movies=movies)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
 
 
 @app.cli.command()  # 注册为命令，可以传入 name 参数来自定义命令
