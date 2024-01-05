c159f4da593a58d56cffd3d984face003236d320
hython4 <hython4@gmail.com>
2024-01-05 06:09:59 +0000

    Add minimal home page

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/hello")
+def hello():
+    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+
+
+@app.route("/user/<name>")
+def user_page(name):
+    return f"User: {name}"
+
+
+@app.route("/test")
+def test_url_for():
+    print(url_for("hello"))
+    print(url_for("user_page", name="chuang"))
+    print(url_for("test_url_for", num=2))
+    return "Test page"
